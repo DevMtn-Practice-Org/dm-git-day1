587359cef3b3254d0fdee68c229a1063972ca3e7
Jeremy R <jeremy@devmounta.in>
2016-08-15 14:49:34 -0600

    changes

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 var journal = ["DM13 rocks!! Woot!!"]
+
+/*
+cd foldername
+cd ..
+cd ~
+ls
+mkdir newfoldername
+touch newFileName.txt
+
+
+git clone
+git status
+git add filename.js
+git commit -m "message"
+git push
+
+*/
